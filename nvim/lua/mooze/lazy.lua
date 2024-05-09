e52476b13dff2c17838e07fb133f9c223cbcd1b3
glagiewka <lagiewka.grzegorz@outlook.com>
2024-05-09 17:52:05 +0200

    chore: reformat

--- nvim/lua/mooze/lazy.lua
+++ nvim/lua/mooze/lazy.lua
@@ -1,45 +1,45 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-	-- telescope
-	"nvim-lua/plenary.nvim",
-	{
-		"nvim-telescope/telescope.nvim", tag = "0.1.6",
-		dependencies = { "nvim-lua/plenary.nvim" }
-	},
+    -- telescope
+    "nvim-lua/plenary.nvim",
+    {
+        "nvim-telescope/telescope.nvim", tag = "0.1.6",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
 
-	-- theme
-	{
-		"rose-pine/neovim",
-		name = "rose-pine"
-	},
+    -- theme
+    {
+        "rose-pine/neovim",
+        name = "rose-pine"
+    },
 
-	-- lsp-zero
-	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-	'neovim/nvim-lspconfig',
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/nvim-cmp',
-	'L3MON4D3/LuaSnip',
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
+    -- lsp-zero
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"
-	},
-	'mbbill/undotree',
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"
+    },
+    'mbbill/undotree',
 
     {
         "L3MON4D3/LuaSnip",
