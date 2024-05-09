fbc57620aafa47dce9fe3789042bcc27efaa9101
glagiewka <lagiewka.grzegorz@outlook.com>
2024-05-09 17:45:02 +0200

    feat: disable search highlight

--- nvim/lua/mooze/remap.lua
+++ nvim/lua/mooze/remap.lua
@@ -3,34 +3,60 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- DAP
 vim.keymap.set("n", "<leader>db", function()
-	require('dap').toggle_breakpoint()
+    require('dap').toggle_breakpoint()
 end)
 
-vim.keymap.set("n", "<leader>dbc", function()
-	require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
+vim.keymap.set("n", "<leader>dc", function()
+    require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))
 end)
 
-vim.keymap.set("n", "<leader>dbl", function()
-	require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
+vim.keymap.set("n", "<leader>dl", function()
+    require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
 end)
 
-vim.keymap.set("n", "<leader>drl", function()
-	require('dap').repl.open()
+vim.keymap.set("n", "<leader>dr", function()
+    require('dap').repl.open()
+end)
+
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+    require('dap.ui.widgets').hover()
+end)
+
+vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+    require('dap.ui.widgets').preview()
+end)
+
+vim.keymap.set('n', '<Leader>df', function()
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.frames)
+end)
+
+vim.keymap.set('n', '<Leader>ds', function()
+    local widgets = require('dap.ui.widgets')
+    widgets.centered_float(widgets.scopes)
 end)
 
 vim.keymap.set("n", "<F5>", function()
-	require('dap').continue()
+    require('dap').continue()
 end)
 
-vim.keymap.set("n", "<F10>", function()
-	require('dap').step_over()
+vim.keymap.set("n", "<F17>", function()
+    require('dap').terminate()
 end)
 
-vim.keymap.set("n", "<F11>", function()
-	require('dap').step_into()
+vim.keymap.set("n", "<F6>", function()
+    require('dap').run_to_cursor()
 end)
 
-vim.keymap.set("n", "<F12>", function()
-	require('dap').step_out()
+vim.keymap.set("n", "<F22>", function()
+    require('dap').step_over()
+end)
+
+vim.keymap.set("n", "<F23>", function()
+    require('dap').step_into()
+end)
+
+vim.keymap.set("n", "<F24>", function()
+    require('dap').step_out()
 end)
 
--- nvim/lua/mooze/set.lua
+++ nvim/lua/mooze/set.lua
@@ -12,8 +12,9 @@ vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
+
+vim.opt.hlsearch = false
 
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = "120"
